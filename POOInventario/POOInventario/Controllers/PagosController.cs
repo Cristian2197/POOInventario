30e4a9adcb8618b987bdbc33bc082d78be3481b2
Cristian2197 <50083819+Cristian2197@users.noreply.github.com>
2020-05-17 18:35:01 -0600

    Fix

---
+++ POOInventario/POOInventario/Controllers/PagosController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AutoMapper;
+using ent = Capa_de_Entidades;
+using dom = Dominio;
+
+namespace POOInventario.Controllers
+{
+    public class PagosController : Controller
+    {
+        // GET: Pagos
+        public ActionResult Index()
+        {
+            var _pagos = new dom.PagosD().PagosList();
+            return View(_pagos);
+        }
+        //Controlador para CREAR pagos
+        [HttpGet]
+        public ActionResult Crear()
+        {
+            var _pagos = new ent.PagosE();
+            return PartialView("Crear", _pagos);
+        }
+        [HttpPost]
+        public ActionResult Crear(ent.PagosE pagos)
+        {
+            new dom.PagosD().CrearPago(pagos);
+            return RedirectToAction("Index");
+        }
+        //Controlador para EDITAR los pagos
+
+        [HttpGet]
+        public ActionResult Editar(int id)
+        {
+            var _pagos = new dom.PagosD().PagosPorID(id);
+            return PartialView("Editar", _pagos);
+        }
+        [HttpPost]
+        public ActionResult Editar(ent.PagosE pagoEditado)
+        {
+            new dom.PagosD().ModificarPago(pagoEditado);
+            return RedirectToAction("Index");
+        }
+    }
+}
